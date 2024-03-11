c7ae6b9f46c2d589d4137e153aaae3342d18b79f
GlennUbly <88552377+GlennUbly@users.noreply.github.com>
2024-03-11 23:18:38 +0000

    Update 50_Conclusions.py
    Conclusions added with images from files

--- pages/50_Conclusions.py
+++ pages/50_Conclusions.py
@@ -1,5 +1,5 @@
 # Streamlit app for PPCI travel times in Kent and Medway
-# 22/02/2024 GU
+# 11/03/2024 GU
 # Page 5 for conclusons on optimal new site configurations 
 
 ##############################################################################
@@ -8,17 +8,90 @@
 #
 ##############################################################################
 
-import geopandas as gpd
-import matplotlib.pyplot as plt
-import pandas as pd
-from bokeh.plotting import ColumnDataSource, figure, output_file, show, output_notebook
-from bokeh.models import LinearInterpolator
-from shapely import wkt
-from shapely.geometry import Point
-import seaborn as sbn
-import numpy as np
-import itertools
+#import geopandas as gpd
+#import matplotlib.pyplot as plt
+#import pandas as pd
+#from bokeh.plotting import ColumnDataSource, figure, output_file, show, output_notebook
+#from bokeh.models import LinearInterpolator
+#from shapely import wkt
+#from shapely.geometry import Point
+#import seaborn as sbn
+#import numpy as np
+#import itertools
 import streamlit as st
-import time
+#import time
+from PIL import Image
+import os
 
-st.write('in progress')
+st.title('Conclusions')
+
+#st.write('Here we show optimal configurations for 1 or 2 sites')
+site_count = st.radio('Select number of additional sites to see '+
+                      'optimal configuration',
+                      ('One', 'Two'),
+                      horizontal = True)
+
+if site_count == 'One' :
+    st.markdown('### The optimal configuration with 1 additional site is '+
+                'the addition of RWF03: The Maidstone Hospital')
+                
+    st.markdown('* Median travel time for Kent and Medway patients reduced '+
+                'from 32 to 18 minutes.')
+    
+    rwf03_kde = Image.open(os.getcwd()+'/RWF03_kde.png')
+    st.image(rwf03_kde)
+    
+    rwf03_times = Image.open(os.getcwd()+'/RWF03_times.png')
+    st.image(rwf03_times)
+    
+    st.markdown('* Travel times would be reduced for 67% of the Kent and '+
+                'Medway patients, based on historic activity.')
+    
+    rwf03_impact = Image.open(os.getcwd()+'/RWF03_impact.png')
+    st.image(rwf03_impact)
+    
+    st.markdown('* Travel times would be reduced to less than the national '+
+                'median for 65% of the Kent and Medway patients, '+
+                'based on historic activity.')
+    
+    rwf03_threshold = Image.open(os.getcwd()+'/RWF03_threshold.png')
+    st.image(rwf03_threshold)
+    
+    st.markdown('* Mean travel time reduction would be 19 minutes.')
+    
+    st.markdown('* Total travel distance (1 way) reduction would be 36,612 '+
+                'km for the period of actuals considered.')
+    st.markdown('* The new maximum travel time would be 35 minutes.')
+    
+if site_count == 'Two' :
+    st.markdown('### The optimal configuration with 2 additional sites is '+
+                'the addition of RWF03: The Maidstone Hospital and RVVKC: '+
+                'Kent & Canterbury Hospital')
+                
+    st.markdown('* Median travel time for Kent and Medway patients reduced '+
+             'from 32 to 16 minutes.')
+    
+    RWF03_RVVKC_kde = Image.open(os.getcwd()+'/RWF03-RVVKC_kde.png')
+    st.image(RWF03_RVVKC_kde)
+    
+    RWF03_RVVKC_times = Image.open(os.getcwd()+'/RWF03-RVVKC_times.png')
+    st.image(RWF03_RVVKC_times)
+    
+    st.markdown('* Travel times would be reduced for 78% of the Kent and '+
+                'Medway patients, based on historic activity.')
+    
+    RWF03_RVVKC_impact = Image.open(os.getcwd()+'/RWF03-RVVKC_impact.png')
+    st.image(RWF03_RVVKC_impact)
+    
+    st.markdown('* Travel times would be reduced to less than the national '+
+                'median for 48% of the Kent and Medway patients, '+
+                'based on historic activity.')
+    
+    RWF03_RVVKC_threshold = Image.open(os.getcwd()+'/RWF03-RVVKC_threshold.png')
+    st.image(RWF03_RVVKC_threshold)
+    
+    st.markdown('* Mean travel time reduction would be 21 minutes.')
+    
+    st.markdown('* Total travel distance (1 way) reduction would be 39,560 '+
+                'km for the period of actuals considered.')
+    st.markdown('* The new maximum travel time would be 35 minutes.')
