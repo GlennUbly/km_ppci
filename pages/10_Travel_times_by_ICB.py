902c013bcf0819be84e7fdd5af7d3da3a80af461
GlennUbly <88552377+GlennUbly@users.noreply.github.com>
2024-07-10 00:54:05 +0100

    Update 10_Travel_times_by_ICB.py
    Added test sentence to end

--- pages/10_Travel_times_by_ICB.py
+++ pages/10_Travel_times_by_ICB.py
@@ -789,4 +789,4 @@ st.write("We observe that Kent and Medway ICB ranks 37 out of 42 for PPCI travel
 
 
 end_full = time.time()
-# st.write('Total time to run '+str(round(end_full - start_full,1)) + ' seconds')
+st.write('test sentence')
