a773675411dc276d36a9a16ef22ab236359ca08f
GlennUbly <88552377+GlennUbly@users.noreply.github.com>
2024-07-09 21:23:20 +0100

    Update 20_Kent_and_Medway_Travel_Times.py
    Remove new cols in get_new_min_times_gdf

--- pages/20_Kent_and_Medway_Travel_Times.py
+++ pages/20_Kent_and_Medway_Travel_Times.py
@@ -416,10 +416,10 @@ def get_new_min_times_gdf(km_lsoa_gdf, sites_orig, sites_new, threshold):
                                                                 'Remains > national median')
                                                                )
     # New columns to show 
-    #site_time_new_list = list(new_min_times_gdf.columns)[2:2+len(sites_new)+len(sites_orig)]
-    #site_time_orig_list = list(new_min_times_gdf.columns)[2:2+len(sites_orig)]
-    #gdf_calculated_min_time['site_of_new_time'] = gdf_calculated_min_time[site_time_new_list].idxmin(axis=1)
-    #gdf_calculated_min_time['closest_site_current'] = gdf_calculated_min_time[site_time_orig_list].idxmin(axis=1)
+    # site_time_new_list = list(gdf_calculated_min_time.columns)[2:2+len(sites_new)+len(sites_orig)]
+    # site_time_orig_list = list(gdf_calculated_min_time.columns)[2:2+len(sites_orig)]
+    # gdf_calculated_min_time['site_of_new_time'] = gdf_calculated_min_time[site_time_new_list].idxmin(axis=1)
+    # gdf_calculated_min_time['closest_site_current'] = gdf_calculated_min_time[site_time_orig_list].idxmin(axis=1)
     return gdf_calculated_min_time
 
 # Function to plot the new GDF with the minimum time for the existing and new sites 
